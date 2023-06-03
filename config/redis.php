168b019c0e9b88da3c9ba4582b946e2be8c3d0b6
itlattice <notice@itgz8.com>
2023-06-03 21:18:43 +0800

    1

---
+++ config/redis.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'host'=>'127.0.0.1', //IP Address
+    'port'=>6379, //Port
+    'password'=>'',
+    'expire'=>0,  //Expiration time
+    'time_out'=>0,
+    'prefix'=>'', //前缀
+    'select'=>0  //选择的库(一般有16个库)
+];
+?>
